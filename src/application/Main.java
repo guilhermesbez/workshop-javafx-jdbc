b0411d39039ceb2fcdd9b6d185c66b7cda95e728
Guilherme Bez <guilhermesbez@gmail.com>
2021-03-31 21:55:13 -0300

    Adicionado View Department

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,14 +10,18 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	
+	//Save reference the principal View
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scroolPane = loader.load();
-			scroolPane.setFitToHeight(true);
-			scroolPane.setFitToWidth(true);
-			Scene mainScene = new Scene(scroolPane);
+			scroolPane.setFitToHeight(true);//Adjust vbox on View
+			scroolPane.setFitToWidth(true);//Adjust vbox on View
+			
+			mainScene = new Scene(scroolPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -26,6 +30,11 @@ public class Main extends Application {
 		}
 	}
 
+	//Method for take reference mainScene
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,17 +1,26 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
-
+	
+	//Adicionando os itens do Menu
 	@FXML
-	private MenuItem menuItenSeller;
+	private MenuItem menuItemSeller;
 	
 	@FXML
 	private MenuItem menuItemDepartament;
@@ -19,26 +28,54 @@ public class MainViewController implements Initializable {
 	@FXML
 	private MenuItem menuItemAbout;
 	
+	//Adicionando os Eventos da tela
 	@FXML
 	public void onMenuItemSellerAction() {
-		System.out.println("onMenuItemSellerAction");
+		loadView("/gui/Seller.fxml");//Informar qual View sera chamada
 	}
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("onMenuItemDepartmentAction");
+		loadView("/gui/DepartmentList.fxml");//Informar qual View sera chamada
 	}
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");//Informar qual View sera chamada
 	}
 	
+	//Adicionado automcaticamente quando coloca Initializable no main
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {		
 	}
 	
-
+	//Função para chamar outra tela - Syncronized garante que ela não sera interrompida
+	public synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			//Show view in principal
+			Scene mainScene = Main.getMainScene();
+			
+			//Criar uma referencia do Vbox
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			//Exclui tudo que tiver nos filhos do VBox, e incluir os filhos
+			Node mainMenu = mainVBox.getChildren().get(0);
+			
+			//Limpar os filhos do VBox
+			mainVBox.getChildren().clear();
+			
+			//Depois de limpar tudo, tem que adicionar um por um
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+		}
+		catch (IOException e){
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
 
 	
 }
